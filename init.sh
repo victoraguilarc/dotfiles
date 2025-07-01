8dc5dbd170f317fe840232d9f5c04bb345143cfb
Victor Aguilar C. <vicobits@gmail.com>
2025-07-01 17:00:38 -0600

    chore: add new commands

--- init.sh
+++ init.sh
@@ -1,9 +1,5 @@
+git clone --bare git@github.com:victoraguilarc/dotfiles.git $HOME/.cfg
 echo "alias dotfiles='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'" >> ~/.zshrc
 source ~/.zshrc
 dotfiles config --local status.showUntrackedFiles no
-
-dotfiles checkout
-
-dotfiles add $HOME/.zshrc $HOME/.gitconfig
-dotfiles commit -m "chore: dotfiles actualizados"
-dotfiles push
+dotfiles pull --rebase
